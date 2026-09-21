4c567185c32ad7772ec00f6aaf98bdc2ebfdcc07
menry <menry4444@gmail.com>
2026-09-20 21:08:07 -0400

    lil tweaks17

--- src/verilog/cpu/cpu_top.sv
+++ src/verilog/cpu/cpu_top.sv
@@ -102,16 +102,6 @@ module cpu_top (
 
 	assign rd_addr = instr[11:7];
 
-    logic load_pending;
-
-    always_comb begin
-        stall_core = halted_o | ~en_i;
-
-        if (load_pending && !dsram_rready_i)
-            stall_core = 1'b1;
-    end
-
-
     always_comb begin
         branch_vld = 1'b0;
         branch_trgt = '0;
@@ -164,19 +154,16 @@ module cpu_top (
             end
 
             //lw
-            if (opcode == 7'b0000011 && funct3 == 3'b010) begin
-                dsram_en_o = 1'b1;
-                mem_addr = rs1_data + $unsigned(imm_i);
-                dsram_addr_o = mem_addr[9:0];
+            7'b0000011: begin
+                if (funct3 == 3'b010) begin
+                    dsram_en_o = 1'b1;
+                    mem_addr   = rs1_data + $unsigned(imm_i);
+                    dsram_addr_o = mem_addr[9:0];
 
-                if (!load_pending) begin
-                    load_pending = 1'b1;
-                end
-
-                if (dsram_rready_i) begin
-                    rd_data = dsram_rdata_i;
-                    rd_write_en = 1'b1;
-                    load_pending = 1'b0;
+                    if (dsram_rready_i) begin
+                        rd_data     = dsram_rdata_i;
+                        rd_write_en = 1'b1;
+                    end
                 end
             end
 
